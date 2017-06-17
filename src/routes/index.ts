9ed16ae40f105e74002d48ab7bc3e50c9b1c39bf
Sante Kotturi <santekotturi@Santes-MacBook-Pro.local>
2017-06-17 13:13:06 -0700

    added cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,29 @@
 import { Router } from 'express';
+import * as cors from 'cors';
 
 const index: Router = Router();
 
-/* GET home page. */
+//options for cors midddleware
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  credentials: false,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  // origin: API_URL,
+  preflightContinue: false
+};
+
+//use cors middleware
+index.use(cors(options));
+
+//enable pre-flight
+index.options("*", cors(options));
+
+
+//add your routes
+
+/* base endpoint */
 index.get('/', function(req, res, next) {
-  res.render('index', { title: 'Visual Studio Code!' });
+  res.send({hello: 'world'})
 });
 
 export default index;
